ca23cf16bfdec32a341f026840e93592b7391504
gosndule <80468848+gosndule@users.noreply.github.com>
2024-09-17 12:39:07 +0200

    fixed dotenv dependency, removed duplicate loaders

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -8,9 +8,7 @@ import { useGetTopProductsQuery } from '../slices/productsApiSlice'
 const ProductCarousel = () => {
   const { data: products, isLoading, error } = useGetTopProductsQuery()
 
-  return isLoading ? (
-    <Loader />
-  ) : error ? (
+  return error ? (
     <Message variant="danger">{error}</Message>
   ) : (
     <>
